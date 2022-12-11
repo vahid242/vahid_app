4ece74ba6df5ecf731367ccbd46a1991512b716e
vahidghaed <vahid.gshf@gmail.com>
2022-12-11 11:39:10 -0500

    add post method

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var express = require("express");
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var nodeMailer = require('nodemailer');
 const { engine } = require("express/lib/application");
 var app     = express();
 
@@ -9,8 +12,15 @@ app.use('/img', express.static(__dirname + 'public/img'))
 
 
 //Set views
+app.engine('.hbs', exphbs.engine({ extname: '.hbs'}));
+app.set('view engine', '.hbs');
 app.set('views', './views')
 app.set('view engine', 'ejs')
+
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
 
 app.get('', (req, res) =>{
     res.render('main')
@@ -22,6 +32,48 @@ app.get('/about', (req, res) =>{
 
 app.get('/contact', (req, res) =>{
     res.render('contact')
+})
+
+app.post('/send', (req, res) =>{
+    const output =`
+        <p>You have a new contact request</p>
+        <h3>Contact Details</h3>
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${req.body.message}</p>
+    `;
+
+    let transporter = nodeMailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'reply.height@gmail.com',
+          pass: 'Myp@ss123',
+        },
+        tls:{
+            rejectUnauthorized:false
+        }
+
+      });
+
+      let mailOption = {
+        from: '"Nodemailer Contact" <reply.height@gmail.com>',
+        to: 'vahid.ghsharaf@gmail.com',
+        subject: 'Node Contact Request',
+        html: output
+      };
+
+      transporter.sendMail(mailOption, (error, info) =>{
+        if(error){
+            return console.log(error)
+        }
+        console.log('Massage send: %s', info.messageId);
+        console.log('Massage URL: %s', nodemailer.getTestMesageUrl(info));
+
+        res.render('contact', {msg:'EMAIL has been sent'})
+      })
 })
 
 const port = process.env.PORT || 3000;
